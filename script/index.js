3eac4a3168a0e019c326e051faaaaf94110e7e4a
SVickyPawar <97332040+SVickyPawar@users.noreply.github.com>
2022-02-26 09:34:31 +0530

    Final
    Complete project

---
+++ script/index.js
@@ -0,0 +1,7 @@
+document.querySelector(".signup").addEventListener("click",function(){
+    window.location.href="signup.html"
+});
+
+document.querySelector(".login").addEventListener("click",function(){
+    window.location.href="login.html"
+});
---
+++ script/login.js
@@ -0,0 +1,7 @@
+document.getElementById("signupbtn").addEventListener("click",function(){
+    window.location.href="signup.html"
+    console.log("Hi")
+})
+document.getElementById("loginbtn").addEventListener("click",function(){
+    window.location.href="login.html"
+})
